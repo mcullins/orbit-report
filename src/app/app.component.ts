1f0a569ab43532d92b0062198d8e9b1a82c3f27f
Megan Cullins <mlcullins@gmail.com>
2019-12-19 15:53:02 -0600

    finished through part 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'oribt-report';
+  title = 'Orbit Report';
+  sourceList: Satellite[];
+
+  constructor(){
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json'; 
+    
+    window.fetch(satellitesUrl).then(function(response){
+      response.json().then(function(data){
+        let fetchedSatellites = data.satellites;
+        for (let i = 0; i < fetchedSatellites.length; i++){
+          this.sourceList.push(new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational));
+        }
+      }.bind(this));
+    }.bind(this));
+ 
+ 
+  }
 }
